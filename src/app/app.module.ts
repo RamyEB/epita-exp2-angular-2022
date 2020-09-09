958ce79ed79c1adc24672f65e3e7f5272e73764f
Ramy <ramydoki@gmail.com>
2020-09-09 13:38:05 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,21 +9,33 @@ import { AppComponent } from './app.component';
 import { BarreDeRechercheComponent } from './barre-de-recherche/barre-de-recherche.component';
 import { VisualisationComponent } from './visualisation/visualisation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BarreDeRechercheComponent,
-    VisualisationComponent
+    VisualisationComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatInputModule,
-    MatButtonModule,
-    BrowserAnimationsModule
+    MatButtonModule, FormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/barre-de-recherche/barre-de-recherche.component.ts
+++ src/app/barre-de-recherche/barre-de-recherche.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./barre-de-recherche.component.css']
 })
 export class BarreDeRechercheComponent implements OnInit {
+  searchName = "";
+
+  recherche() {
+    console.log(this.searchName)
+    this.searchName = ""
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
+
+// https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=7f1dcb2ca9f1186a88a3a32ff1a64971&tags=sport&format=json&nojsoncallback=1&api_sig=83bfc68817e674fd392e7333bf9a26b8
+// https://www.flickr.com/services/api/misc.urls.html
